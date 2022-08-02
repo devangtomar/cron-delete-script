dc28ab7cbce96606af13422e43bf2d51ac87d14c
d0t077o <Devang.Tomar@walmart.com>
2022-08-02 10:19:01 +0530

    Adding new logic

--- regex-find.sh
+++ regex-find.sh
@@ -1,1 +1,5 @@
 find . -type f -mtime +30 | grep -iE '(.*(\_(\d){4}\_(\d){2}\_(\d){2}\_).*(sas7bdat|log))' | xargs -I {} rm -rf {}
+
+# New logic
+
+find . -regextype sed -regex ".*[0-9]\{4\}\_[0-9]\{2\}\_[0-9]\{2\}\_[0-9]\{2\}\_.*.txt"
